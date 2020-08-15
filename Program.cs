d747a7f01ed74e42a1a43919d54fb5a8f90e29c0
Mikael Bertze <mikael@bertze.com>
2020-08-16 00:25:08 +0200

    First version

---
+++ Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using NuGet.Common;
+using NuGet.Packaging;
+using NuGet.Protocol.Core.Types;
+using NuGet.Versioning;
+
+namespace NuGet.Protocol.Samples
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Search packages..");
+            await SearchPackages();
+        }
+
+        public static async Task SearchPackages()
+        {
+            ILogger logger = NullLogger.Instance;
+            CancellationToken cancellationToken = CancellationToken.None;
+            SourceRepository repository = Repository.Factory.GetCoreV3("https://api.nuget.org/v3/index.json");
+            PackageSearchResource searchResource = await repository.GetResourceAsync<PackageSearchResource>();
+            FindPackageByIdResource findPackageresource = await repository.GetResourceAsync<FindPackageByIdResource>();
+            SearchFilter searchFilter = new SearchFilter(includePrerelease: false);
+
+            var downloadTasks = new List<Task>();
+
+            bool done = false;
+            int stepSize = 10;
+            int start = 0;
+            int counter = 0;
+            int maxPackages = 10000;
+
+            while(!done)
+            {
+                IEnumerable<IPackageSearchMetadata> results = await searchResource.SearchAsync(
+                    "owner:dotnetframework",
+                    searchFilter,
+                    skip: start,
+                    take: stepSize,
+                    logger,
+                    cancellationToken);
+
+                if (results.Any())
+                {
+                    foreach (IPackageSearchMetadata package in results)
+                    {
+                        var versions = await package.GetVersionsAsync();
+                        Console.WriteLine("-----------------------------------------------");
+                        Console.WriteLine($"Package {package.Identity.Id}");
+                        Console.WriteLine($"Versions: { string.Join(", ", versions.Select(x => x.Version.ToNormalizedString())) }");
+
+                        foreach(var version in versions)
+                        {
+                            var filename = $"{package.Identity.Id}.{version.Version.ToString()}.nupkg";
+                            var cache = new SourceCacheContext();
+                            var downloader = await findPackageresource.GetPackageDownloaderAsync(package.Identity, cache, logger, cancellationToken);
+                            downloadTasks.Add(downloader.CopyNupkgFileToAsync(Path.Join("packages", filename), cancellationToken));
+                        }
+                        counter++;
+                        if (counter >= maxPackages)
+                        {
+                            done = true;
+                            break;
+                        }
+                    }
+                    start += stepSize;
+                }
+                else{
+                    done = true;
+                }
+            }
+            
+            Console.WriteLine($"Done searching for packages. Packages found: {counter}");
+            Console.WriteLine("Waiting for downloads to finish...");
+            Task.WaitAll(downloadTasks.ToArray());
+            Console.WriteLine("DONE!");
+        }
+    }
+}
